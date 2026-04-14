ab952dafe5dd504c18acb5dec4c379c6ca6778ff
AdamDakhl2A <asdakhlallah567@gmail.com>
2026-04-14 19:17:54 -0400

    livestream from anywhere update
    works but we ran out of bandwidth.
    
    explore other options for bandwidth

---
+++ Codes/Raspi/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getDatabase, ref, update, onValue, get } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNS_uT-T1k0S_T9Ofa7iK0l3Ciu9Embtc",
+  authDomain: "plant-enclosure.firebaseapp.com",
+  databaseURL: "https://plant-enclosure-default-rtdb.firebaseio.com",
+  projectId: "plant-enclosure",
+  storageBucket: "plant-enclosure.firebasestorage.app",
+  messagingSenderId: "8601368106",
+  appId: "1:8601368106:web:515c7bbac13a6324b73553",
+  measurementId: "G-NBSVHWE9S6"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, update, onValue, get };
